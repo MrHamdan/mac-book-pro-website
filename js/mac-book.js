5a442d2a30d95deb839f5544604b8faea331eb1b
MrHamdan <mr.hamdanahmed@gmail.com>
2021-08-21 18:14:38 +0600

    adding comments

--- js/mac-book.js
+++ js/mac-book.js
@@ -1,3 +1,4 @@
+// Memory, Storage, Delivery Update Cost
 function updateCost(product, cost) {
     const selectedProductCost = document.getElementById(product + '-cost')
     selectedProductCost.innerText = cost;
@@ -6,7 +7,6 @@ function updateCost(product, cost) {
 
 
 // Calculate Total Price
-
 function calculateTotal() {
     const totalPrice = document.getElementById('total-price');
     const totalPriceUsingPromoCode = document.getElementById('total-cost');
@@ -16,6 +16,8 @@ function calculateTotal() {
 
 
     const total = parseInt(memoryCost) + parseInt(storageCost) + parseInt(deliveryCost) + 1299;
+
+    // Update Total Price
     totalPrice.innerText = total;
     totalPriceUsingPromoCode.innerText = total;
 
@@ -24,7 +26,6 @@ function calculateTotal() {
 
 
 // Handle Memory Events
-
 document.getElementById('8gb-button').addEventListener('click', function () {
 
     updateCost('memory', 0);
@@ -38,7 +39,6 @@ document.getElementById('16gb-button').addEventListener('click', function () {
 });
 
 // Handle Storage Events
-
 document.getElementById('256gb-button').addEventListener('click', function () {
 
     updateCost('storage', 0);
@@ -61,7 +61,6 @@ document.getElementById('1tb-button').addEventListener('click', function () {
 
 
 // Handle Delivery Events
-
 document.getElementById('free-delivery').addEventListener('click', function () {
 
     updateCost('delivery', 0);
@@ -79,7 +78,6 @@ document.getElementById('paid-delivery').addEventListener('click', function () {
 
 
 // Handle Apply Promo Code Events For Discounted Total
-
 document.getElementById('apply-button').addEventListener('click', function () {
     const getPromoCode = document.getElementById('input-field');
     const promoCode = getPromoCode.value;
